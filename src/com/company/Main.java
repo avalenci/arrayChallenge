5719338f46c893af28446037a165bae26ab8e21c
avalenci <ansmeew@gmail.com>
2016-10-27 23:22:51 -0400

    Second commit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,20 +8,31 @@ public class Main {
         int a = -1;
         int b;
         int grades[] = new int[100];
+        String names[] = new String[100];
         while (true) {
             System.out.println("Enter your name.");
-            String name = i.nextLine();
+            String name = i.next();
             a++;
             b = a + 1;
-            System.out.println("Enter your grade.");
+            System.out.println("Enter your grade or -1 to exit.");
             int g = i.nextInt();
             if (g == -1)
                 break;
             grades[a] = g;
+            names[a] = name;
             int sum = 0;
-            for (int n = 0; n < b; n++)
+            for (int n = 0; n < b; n++) {
                 sum = sum + grades[n];
-            System.out.println(sum / b);
+            }
+            int average = sum / b;
+            int x = -1;
+            int count = 0;
+            while (x < a) {
+                count++;
+                x++;
+                System.out.print(names[x] + ", ");
+            }
+            System.out.println("Class average: " + average);
         }
     }
 }
